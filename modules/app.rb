fb97024aa319814285886dba6d3cd80efa48e151
Kender <kenderromain@gmail.com>
2023-04-26 11:02:54 -0400

    Preserve author data in json file.

--- modules/app.rb
+++ modules/app.rb
@@ -1,8 +1,11 @@
 require_relative './book_module'
+require_relative './author'
 
 class App
   include BookListing
+  include AuthorModule
   def initialize()
     @books = []
+    @authors = load_authors
   end
 end
---
+++ modules/author.rb
@@ -0,0 +1,52 @@
+require_relative '../classes/author'
+require 'json'
+module AuthorModule
+  DB = './db/authors.json'.freeze
+
+  def list_all_authors
+    puts "\n"
+    if @authors.empty?
+      puts 'Author list is empty.'
+    else
+      @authors.each_with_index do |author, index|
+        puts "#{index})  ID: #{author.id}, First name: #{author.first_name}, Last name: #{author.last_name}"
+      end
+    end
+    puts "\n"
+  end
+
+  def add_author
+    puts "\n"
+    puts 'Please provide these information :'
+    print 'First name : '
+    first_name = gets.chomp
+    print 'Last name : '
+    last_name = gets.chomp
+    new_author = Author.new(first_name, last_name)
+    @authors << new_author
+    puts 'Author created successfully'
+    preseve_author
+    puts "\n"
+  end
+
+  def preseve_author
+    author_ojects = []
+    @authors.each do |author|
+      author_ojects << { id: author.id, first_name: author.first_name, last_name: author.last_name }
+    end
+    File.write(DB, author_ojects.to_json)
+  end
+
+  def load_authors
+    authors = []
+    if File.exist?(DB) && !File.empty?(DB)
+      data = JSON.parse(File.read(DB))
+      data.each do |author|
+        new_author = Author.new(author['first_name'], author['last_name'])
+        new_author.id = author['id']
+        authors << new_author
+      end
+    end
+    authors
+  end
+end
