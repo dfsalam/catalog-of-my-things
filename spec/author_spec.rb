d6bda9e59022ebc70def446af0be8a4ed079a859
Kender <kenderromain@gmail.com>
2023-04-26 15:21:45 -0400

    Fix linter errors.

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,7 +2,7 @@ require_relative '../classes/author'
 
 describe Author do
   before :each do
-    @author = Author.new('Kender','Romain')
+    @author = Author.new('Kender', 'Romain')
   end
 
   describe '#new' do
@@ -10,7 +10,7 @@ describe Author do
       expect(@author).to be_an_instance_of Author
     end
   end
-  
+
   describe '#first_name' do
     it 'Should return the correct first_name' do
       first_name = @author.first_name
@@ -30,7 +30,7 @@ describe Author do
   describe '#add_item' do
     it 'Should return an empty array' do
       game = Game.new('Multi-player', '2023-04-13', '2020-05-14')
-      
+
       @author.add_item(game)
       items = @author.items
 
@@ -38,5 +38,4 @@ describe Author do
       expect(items.include?(game)).to be true
     end
   end
-
 end
