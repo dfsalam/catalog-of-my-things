7a63185d047f97c9e332dd8c4b2c1afeea5793d8
Kender <kenderromain@gmail.com>
2023-04-26 11:30:35 -0400

    Implement add_game method.

--- modules/game.rb
+++ modules/game.rb
@@ -1,4 +1,5 @@
 require_relative '../classes/game'
+require_relative '../classes/author'
 require 'json'
 module GameModule
     DB = './db/games.json'
@@ -16,17 +17,35 @@ module GameModule
         puts "\n"
     end
     
+    def initialize(multiplayer, last_play_at, published_date, archived: false)
+        super(published_date, archived: archived)
+        @multiplayer = multiplayer
+        @last_play_at = last_play_at
+      end
     def add_game
+        # GAME
+        puts "\n"
+        puts 'Please provide the information about the game :'
+        print 'Multilayer : '
+        multi_player = gets.chomp
+        print 'Publish Date (Ex: 2023-04-13) : '
+        published_date = gets.chomp
+        print 'Last Play Date (Ex: 2023-04-13): '
+        last_play_date = gets.chomp
+        new_game = Game.new(multi_player,last_play_date,published_date)
+        @games << new_game
+
+        # AUTHOR
         puts "\n"
         puts 'Please provide these information :'
         print 'First name : '
         first_name = gets.chomp
         print 'Last name : '
         last_name = gets.chomp
-        new_game = game.new(first_name,last_name)
-        @games << new_game
-        puts 'game created successfully'
-        preseve_game
+        new_author = Author.new(first_name,last_name)
+        @authors << new_author
+        puts 'Game created successfully'
+        # preseve_game
         puts "\n"
     end
 
