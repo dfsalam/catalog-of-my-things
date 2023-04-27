4a1a278859883f53b8af2609e745b41bf5aea476
dfsalam <dfsalam@gmail.com>
2023-04-27 02:31:00 -0500

    Fix linter errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -2,21 +2,21 @@ require_relative '../classes/genre'
 require_relative '../classes/music_album'
 
 describe Genre do
-  describe "#initialize" do
-    it "creates a new genre with a name and empty items list" do
-      genre = Genre.new("Classic")
-      expect(genre.genre_name).to eq("Classic")
+  describe '#initialize' do
+    it 'creates a new genre with a name and empty items list' do
+      genre = Genre.new('Classic')
+      expect(genre.genre_name).to eq('Classic')
       expect(genre.items).to eq([])
     end
 
-    it "generates a random ID for each new genre" do
-      genre1 = Genre.new("Rap")
-      genre2 = Genre.new("Rock")
+    it 'generates a random ID for each new genre' do
+      genre1 = Genre.new('Rap')
+      genre2 = Genre.new('Rock')
       expect(genre1.id).not_to eq(genre2.id)
     end
   end
 
-  describe "#add_item" do
+  describe '#add_item' do
     it "adds an item to the genre's items list and sets the item's genre to itself" do
       genre = Genre.new('Rock')
       album = MusicAlbum.new('1985-01-01', 'Rock')
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,39 +1,39 @@
 require_relative '../classes/music_album'
 
 describe MusicAlbum do
-  describe "#initialize" do
-    it "creates a new music album with a published date and genre, and sets on_spotify to false by default" do
-      album = MusicAlbum.new('1985-01-01', "Rock")
+  describe '#initialize' do
+    it 'creates a new music album with a published date and genre, and sets on_spotify to false by default' do
+      album = MusicAlbum.new('1985-01-01', 'Rock')
       expect(album.published_date).to eq('1985-01-01')
-      expect(album.genre).to eq("Rock")
+      expect(album.genre).to eq('Rock')
       expect(album.on_spotify).to be false
     end
 
-    it "allows on_spotify to be set to true on initialization" do
-      album = MusicAlbum.new('1985-01-01', "Pop", on_spotify: true)
+    it 'allows on_spotify to be set to true on initialization' do
+      album = MusicAlbum.new('1985-01-01', 'Pop', on_spotify: true)
       expect(album.on_spotify).to be true
     end
 
-    it "allows archived to be set to true on initialization" do
-      album = MusicAlbum.new('1985-01-01', "Classical", archived: true)
+    it 'allows archived to be set to true on initialization' do
+      album = MusicAlbum.new('1985-01-01', 'Classical', archived: true)
       expect(album.archived).to be true
     end
   end
 
-  describe "#can_be_archived?" do
-    it "returns false if the album is not on Spotify or has been published less than 10 years ago" do
-      album = MusicAlbum.new("2010-01-01", "Electronic", on_spotify: false, archived: false)
+  describe '#can_be_archived?' do
+    it 'returns false if the album is not on Spotify or has been published less than 10 years ago' do
+      album = MusicAlbum.new('2010-01-01', 'Electronic', on_spotify: false, archived: false)
       expect(album.can_be_archived?).to be false
     end
 
-    it "returns true if the album is on Spotify and has been published more than 10 years ago" do
-      album = MusicAlbum.new("2010-01-01", "Hip-hop", on_spotify: true, archived: false)
+    it 'returns true if the album is on Spotify and has been published more than 10 years ago' do
+      album = MusicAlbum.new('2010-01-01', 'Hip-hop', on_spotify: true, archived: false)
       expect(album.can_be_archived?).to be true
     end
 
-    it "returns false if the album is on Spotify but has been published less than 10 years ago" do
-      album = MusicAlbum.new('2020-01-01', "Jazz", on_spotify: true, archived: false)
+    it 'returns false if the album is on Spotify but has been published less than 10 years ago' do
+      album = MusicAlbum.new('2020-01-01', 'Jazz', on_spotify: true, archived: false)
       expect(album.can_be_archived?).to be false
-    end    
+    end
   end
 end
